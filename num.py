64c51bc396409a6629eba1d344c6a11bb7bf5fd9
Briano25 <obuttbrian23@gmail.com>
2025-04-18 09:33:01 +0300

    First Code

---
+++ num.py
@@ -0,0 +1,30 @@
+n=11
+print(n)
+
+import statistics
+
+scores = [78, 82, 91, 65, 88, 77, 93]
+
+print("Mean:", statistics.mean(scores))
+print("Median:", statistics.median(scores))
+print("Standard Deviation:", statistics.stdev(scores))
+#print("Standard Deviation:", statistics.average(scores))
+
+
+n=23
+m=5
+
+quotient=z=n//m
+remainder=p=n%m
+
+print('The quotient is:',z)
+print('\nThe remainder is:',p)
+
+import numpy as np
+
+n=np.array([1,2,3,4])
+print(n)
+
+
+import pandas as pd
+print(pd)
